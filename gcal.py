8ca6c9c416b4a429d7db287294e8f677faa8edad
Thomas <ttforsd@gmail.com>
2023-08-29 13:57:28 +0100

    restored orginal flow

--- gcal.py
+++ gcal.py
@@ -35,7 +35,7 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_console()
+            creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
@@ -86,7 +86,7 @@ def write2gcal(event_dict):
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_console()
+            creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
